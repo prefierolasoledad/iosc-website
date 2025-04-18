04574719088a4f48055f0b2ed811b2f4b4c889f5
prefierolasoledad <karanbhatt9900@gmail.com>
2025-04-18 20:10:47 +0530

    remove some unwanted code

--- src/components/heroSecton.jsx
+++ src/components/heroSecton.jsx
@@ -10,64 +10,6 @@ const orbitron = Orbitron({
     subsets: ['latin'],
   })
 
-// const HeroSection=()=>{
-//     return(
-//         <section className="w-full h-[200vh] md:h-[100vh] overflow-hidden bg-black flex flex-col-reverse md:flex-row justify-center">
-//             <div className="w-full md:w-[50%] h-full bg-no-repeat bg-contain flex justify-center"
-//                 style={{
-//                     backgroundImage: `url(${HEROSECTIONIMAGE.src})`,
-//                     backgroundSize:'130% 130%',
-//                     backgroundPosition:'60% 30%'
-//                 }}
-//             />
-
-//             <div className="md:w-[60%] lg:mr-20 text-center md:text-left w-full h-full bg-no-repeat pt-30 md:pt-20 xl:pt-14 text-white" >
-//                 <div className="relative flex items-center justify-center w-full">
-//                     {/* Left Star Background */}
-//                     <div
-//                     className="absolute md:left-[-120px] left-[-20px] mt-5 md:mt-10 w-50 h-50 md:w-70 md:h-70 z-0 animate-sprinkle filter brightness-150"
-//                     style={{
-//                         backgroundImage: `url(${STARS.src})`,
-//                         backgroundRepeat: 'no-repeat',
-//                         backgroundSize: 'contain',
-//                         opacity: 0.8,
-//                     }}
-//                     />
-
-//                     {/* Right Star Background */}
-//                     <div
-//                     className="absolute right-[-140px] mt-4 w-70 h-70 z-0 animate-sprinkle filter brightness-150 delay-1000"
-//                     style={{
-//                         backgroundImage: `url(${STARS.src})`,
-//                         backgroundRepeat: 'no-repeat',
-//                         backgroundSize: 'contain',
-//                         opacity: 0.8,
-//                     }}
-//                     />
-//                     {/* Main Heading */}
-//                     <h1 className={`text-sky-400 text-5xl md:text-7xl  xl:text-9xl font-extrabold text-center relative z-10 ${orbitron.className}`}>
-//                         WE ARE<br/> <p className="text-orange-600">IoSC-EDC </p>
-//                     </h1>
-//                 </div>
-//                 <p className="pt-10 md:pr-10 pr-5 pl-5 text-center md:pl-0 lg:text-lg">
-//                     The <span className="text-orange-600 font-bold">IoSC</span>  is a community full of people who love technology oriented towards innovative 
-//                     development in various fields-from design, system integration, game development, robotics, web, management, etc-and 
-//                     build a platform through which <span className="text-orange-600 font-bold">they will learn, try their hands-on experience, and excel.</span>
-//                 </p>
-//                 <p className="pt-5 md:pr-10 pr-5 pl-5 text-center md:pl-0 lg:text-lg">
-//                     Our mission is to bring <span className="text-orange-600 font-bold">hands-on education</span> based on collaboration and real-life problem-solving through workshops, 
-//                     hackathons, coding competitions, and networking sessions. With those, we equip our own with contemporary skills and 
-//                     industry insight.
-//                 </p>
-//                 <p className="pt-5 md:pr-10 pr-5 pl-5 text-center md:pl-0 lg:text-lg">
-//                     Whether you're a <span className="text-orange-600 font-bold">developer, designer, or tech strategist</span> just starting out or wanting to learn from other peers in the 
-//                     field, our club welcomes anyone interested in learning and  innovating.
-//                 </p>
-//             </div>
-//         </section>
-//     );
-// }
-
 const HeroSection=()=>{
     return(
         <section className="bg-black w-full h-[100vh] overflow-hidden">
