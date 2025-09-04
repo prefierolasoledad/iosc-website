bc933dc7d86be5d6a1a1c70c3159125d3c6b4937
prefierolasoledad <karanbhatt9900@gmail.com>
2025-09-04 17:09:31 +0530

    event details changed

--- src/components/eventRegistrationForm.jsx
+++ src/components/eventRegistrationForm.jsx
@@ -106,7 +106,7 @@ const handleSubmit = async () => {
         <div className="fixed inset-0 bg-black/30 bg-opacity-60 flex items-center justify-center z-50">
           <div className="bg-white text-black rounded-xl p-8 max-w-sm w-full shadow-2xl text-center">
             <h3 className="text-xl font-bold text-blue-500 mb-4">Registration Successful!</h3>
-            <p className="mb-6">Thank you for registering. We've sent you the event details via email.</p>
+            <p className="mb-6">Thank you for registering.</p>
             <button
               onClick={() => setShowSuccessPopup(false)}
               className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
